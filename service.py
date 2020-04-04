d52ee8c9951380404280a7beaff41e11d3c18534
snoopyjoe <tgarner14@gmail.com>
2020-04-04 15:30:01 -0700

    PVR crashing fix
    PVR was still crashing while watching live TV. Moved PVR On/Off to service.py module

--- service.py
+++ service.py
@@ -1,6 +1,14 @@
-import xbmcaddon
+import xbmcaddon, xbmc
 from resources.lib.globals import *
 from resources.lib.service.slinger import Slinger
 
 if USE_SLINGER:
+    if SETTINGS.getSetting('Enable_EPG') == 'true':
+        pvr_toggle_off = '{"jsonrpc": "2.0", "method": "Addons.SetAddonEnabled", "params": ' \
+                         '{"addonid": "pvr.iptvsimple", "enabled": false}, "id": 1}'
+        xbmc.executeJSONRPC(pvr_toggle_off)
+        xbmc.sleep(1000)
+        pvr_toggle_on = '{"jsonrpc": "2.0", "method": "Addons.SetAddonEnabled", "params": ' \
+                        '{"addonid": "pvr.iptvsimple", "enabled": true}, "id": 1}'
+        xbmc.executeJSONRPC(pvr_toggle_on)
     Slinger()
