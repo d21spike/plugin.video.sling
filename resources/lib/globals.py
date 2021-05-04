81130f6cfc7ed2ae1c194f91b734d9a299e6ec26
d21spike <d21spike@gmail.com>
2021-05-04 14:44:34 -0400

    Add resilient function

--- resources/lib/globals.py
+++ resources/lib/globals.py
@@ -226,5 +226,14 @@ def subscribedChannel(self, channel_guid):
 
     return subscribed
 
+def createResilientSession():
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    return session
+
 from resources.lib.classes.channel import Channel
 from resources.lib.classes.show import Show
